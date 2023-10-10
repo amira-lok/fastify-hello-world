fde7bc897621f6c4e2cd0ba7e49c27415573ec6a
Alvaro Mira <alvaro.mira.del.amo@lokalise.com>
2023-10-10 17:04:10 +0200

    Added two additional routes

--- app.js
+++ app.js
@@ -1,12 +1,73 @@
 const port = process.env.PORT || 3000;
 const host = ("RENDER" in process.env) ? `0.0.0.0` : `localhost`;
 
+//const path = require('path')
+//const AutoLoad = require('fastify-autoload')
+
 const fastify = require('fastify')({
   logger: true
 })
 
-fastify.get('/', function (request, reply) {
-  reply.type('text/html').send(html)
+fastify.get('/', async function (request, reply) {
+  await reply.send({ hello: 'world' })
+})
+
+fastify.post('/preprocess', async function (request, reply) {
+  if (!request.body) {
+    return { error: 'Invalid request: Missing body' }
+  }
+
+  // Get the payload from the request:
+  const payload = request.body
+
+  // @todo: validation
+
+  // @todo: introduce timeout
+
+  // Iterate over the keys in the collection:
+  for (const [keyId, keyValue] of Object.entries(payload.collection.keys)) {
+    // Iterate over the translations in the key:
+    for (const [lang, v] of Object.entries(keyValue.translations)) {
+      // Process the value of the translation:
+      payload.collection.keys[keyId].translations[lang].translation = v.translation.replace(
+        '&amp;nbsp;',
+        '&nbsp;',
+      )
+    }
+  }
+
+  // @todo: validate processed values
+
+  await reply.send(payload)
+})
+
+fastify.post('/postprocess', async function (request, reply) {
+  if (!request.body) {
+    return { error: 'Invalid request: Missing body' }
+  }
+
+  // Get the payload from the request:
+  const payload = request.body
+
+  // @todo: validation
+
+  // @todo: introduce timeout
+
+  // Iterate over the keys in the collection:
+  for (const [keyId, keyValue] of Object.entries(payload.collection.keys)) {
+    // Iterate over the translations in the key:
+    for (const [lang, v] of Object.entries(keyValue.translations)) {
+      // Process the value of the translation:
+      payload.collection.keys[keyId].translations[lang].translation = v.translation.replace(
+        '&nbsp;',
+        '&amp;nbsp;',
+      )
+    }
+  }
+
+  // @todo: validate processed values
+
+  await reply.send(payload)
 })
 
 fastify.listen({host: host, port: port }, function (err, address) {
@@ -16,6 +77,7 @@ fastify.listen({host: host, port: port }, function (err, address) {
   }
 })
 
+/*
 const html = `
 <!DOCTYPE html>
 <html>
@@ -73,4 +135,4 @@ const html = `
     </section>
   </body>
 </html>
-`
+`*/
